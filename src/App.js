114b79ede0d66035765f72beeb14358c7a7abd8e
joyfultiger555 <114970320+joyfultiger555@users.noreply.github.com>
2022-12-02 21:38:00 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import {PoliticianItem} from "./components/PoliticianItem.js"
-import { useEffect, useState } from 'react';
-import { Checkbox, Collapse } from 'antd';
-import { propTypes } from "react-bootstrap/esm/Image";
-import CheckBox from '/Users/noahatanda/Desktop/uiux/Development-Anonymous/src/components/CheckBox.js'
+
 
 import Display from '/Users/noahatanda/Desktop/uiux/Development-Anonymous/src/components/Display.js'
 function App() {
